9cca8ef44caf7d6c9c708a0511b5bffb9b10a3ff
Arkadiusz Drężek <drezek.arkadiusz.pawel@gmail.com>
2023-03-08 10:23:51 +0100

    Dodanie wstawiania akcji

--- Zadanie_8504.sql
+++ Zadanie_8504.sql
@@ -1,16 +1,36 @@
+drop table if exists #sp_id 
+
 ;with zwroty_korespondencji_zgony as (
 select
 COUNT(ak_id) over (partition by ak_sp_id order by (select 1)) as ilosc_rezultatow,
 sp_numer,
-ak_sp_id as sp_id,
-ak_id,
-re_komentarz
-from sprawa
-join akcja on ak_sp_id=sp_id and ak_akt_id=329	
-join rezultat on re_ak_id=ak_id and re_ret_id=70 
+ak_sp_id as sp_id
+from dm_data_bps..sprawa
+join dm_data_bps..akcja on ak_sp_id=sp_id and ak_akt_id=329	
+join dm_data_bps..rezultat on re_ak_id=ak_id and re_ret_id=70 
 )
 
-select * from zwroty_korespondencji_zgony where ilosc_rezultatow>=2
+select 
+distinct sp_id into #sp_id 
+from zwroty_korespondencji_zgony 
+where ilosc_rezultatow>=2
 and not exists (
-select 1 from akcja a where a.ak_akt_id in (324,1349) and a.ak_sp_id=sp_id
+select 1 from dm_data_bps..akcja a where a.ak_akt_id in (324,1349) and a.ak_sp_id=sp_id
 )
+
+drop table if exists #ak_id
+create table #ak_id (ak_id int)
+
+merge dm_data_bps..akcja as target using (
+select sp_id from #sp_id
+) as source on 1=0
+when not matched then insert
+([ak_akt_id], [ak_sp_id], [ak_kolejnosc], [ak_interwal], [ak_zakonczono], [ak_pr_id], [ak_publiczna])
+values (1349, sp_id, 0,0,getdate(),5,1)
+output inserted.ak_id into #ak_id;
+
+insert into dm_data_bps..rezultat
+([re_ak_id],[re_data_planowana], [re_us_id_planujacy], [re_data_wykonania], [re_us_id_wykonujacy], [re_konczy])
+select ak_id, GETDATE(), 5, GETDATE(), 5, 1 from #ak_id
+
+
